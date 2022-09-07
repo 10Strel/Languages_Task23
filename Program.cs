51142de6b67f8af487d6b5640313aae983a5ce03
10Strel <manowartenarrows@gmail.com>
2022-09-07 21:55:41 +0300

    Final commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите N:");
+
+bool res = int.TryParse(Console.ReadLine(), out int n);
+
+if (!res)
+{
+    Console.WriteLine("Ошибка ввода.\r\nВыполнение программы будет остановлено.");
+    return;
+}
+
+for (int i = 1; i <= n; i++)
+{
+    double tmp = Math.Pow(i, 3);
+    Console.Write($"{(i == 1 ? n + " -> " + tmp : ", " + tmp)}");
+}
+
